83ac39665078399637a26d0db42078efdad1e4d8
Felipe Axtell <felipeaxtell@gmail.com>
2017-06-18 19:57:50 -0400

    Add create user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,12 +15,23 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new( user_params )
+    if @user.save
+      redirect_to users_path
+    else
+      render :new
+    end
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name,:last_name,:password,:profile_id)
   end
 
 end
